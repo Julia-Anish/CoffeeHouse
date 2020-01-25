a88fc4a85801b5b80d1909904b7511cf1a5bbb73
Julia Anish <julia.anishchenko@gmail.com>
2020-01-25 15:16:56 +0200

    Add 3 new Activity. Created and assigned a listener for menu items

--- app/src/main/java/com/example/coffeehouse/MainActivity.java
+++ app/src/main/java/com/example/coffeehouse/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.coffeehouse;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> listView, View itemView, int position, long id) {
+                if (position==0){
+                    Intent intent = new Intent(MainActivity.this, DrinkMenu.class);
+                    startActivity(intent);
+                }else if(position==1){
+                    Intent intent = new Intent(MainActivity.this,FoodMenu.class);
+                    startActivity(intent);
+                }else {
+                    Intent intent = new Intent(MainActivity.this,ProfitableOffer.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        ListView listView = (ListView)findViewById(R.id.listView);
+        listView.setOnItemClickListener(itemClickListener);
     }
 }
