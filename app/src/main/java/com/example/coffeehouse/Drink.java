c2e1ab74ebccf8d54c85ca30d85369de01bae7f7
Julia Anish <julia.anishchenko@gmail.com>
2020-01-27 18:23:16 +0200

    Added pictures to the class. Created ParticularDrink activity. Completed code to display information about a specific drink

--- app/src/main/java/com/example/coffeehouse/Drink.java
+++ app/src/main/java/com/example/coffeehouse/Drink.java
@@ -3,18 +3,20 @@ package com.example.coffeehouse;
 public class Drink {
     private String name;
     private String description;
+    private  int imageResourceId;
 
 
     public static final Drink [] drinks = {
-            new Drink("Latte", "It is a coffee-based drink made primarily from espresso and steamed milk. It consists of one-third espresso, two-thirds heated milk and about 1cm of foam. The foam can be poured in such a way to create a picture."),
-            new Drink("Cappuccino", "It is a coffee-based drink made primarily from espresso and milk. It consists of one-third espresso, one-third heated milk and one-third milk foam"),
-            new Drink("Amaretto", "It is a liqueur with an almond flavor, but surprisingly, it may or may not contain almonds. The standard base of the liqueur is primarily made from either apricot pits or almonds or both."),
-            new Drink("Green Tea","It is a type of tea that is made from Camellia sinensis leaves and buds that have not undergone the same withering and oxidation process used to make oolong teas and black teas. Green tea originated in China, but its production and manufacture has spread to other countries in East Asia.")
+            new Drink("Latte", "It is a coffee-based drink made primarily from espresso and steamed milk. It consists of one-third espresso, two-thirds heated milk and about 1cm of foam. The foam can be poured in such a way to create a picture.", R.drawable.latte),
+            new Drink("Cappuccino", "It is a coffee-based drink made primarily from espresso and milk. It consists of one-third espresso, one-third heated milk and one-third milk foam", R.drawable.cappuccino),
+            new Drink("Amaretto", "It is a liqueur with an almond flavor, but surprisingly, it may or may not contain almonds. The standard base of the liqueur is primarily made from either apricot pits or almonds or both.",R.drawable.amaretto),
+            new Drink("Green Tea","It is a type of tea that is made from Camellia sinensis leaves and buds that have not undergone the same withering and oxidation process used to make oolong teas and black teas. Green tea originated in China, but its production and manufacture has spread to other countries in East Asia.",R.drawable.greentea)
 
     };
-    private Drink (String name, String description){
+    private Drink (String name, String description, int imageResourceId){
         this.name = name;
         this.description = description;
+        this.imageResourceId = imageResourceId;
     }
     public String getName(){
         return name;
@@ -22,6 +24,10 @@ public class Drink {
     public String getDescription(){
         return description;
     }
+    public int getImageResourceId(){
+        return imageResourceId;
+    }
+
 
     public String toString(){
         return this.name;
--- app/src/main/java/com/example/coffeehouse/DrinkMenu.java
+++ app/src/main/java/com/example/coffeehouse/DrinkMenu.java
@@ -2,7 +2,10 @@ package com.example.coffeehouse;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -22,5 +25,17 @@ public class DrinkMenu extends AppCompatActivity {
         ListView listDrinks = (ListView)findViewById(R.id.listDrinks);
         listDrinks.setAdapter (listAdapter);
 
+        AdapterView.OnItemClickListener itemClickListener = new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> listDrinks, View itemView, int position, long id) {
+                Intent intent = new Intent(DrinkMenu.this, ParticularDrink.class);
+                intent.putExtra(ParticularDrink.EXTRA_DRINKID, (int) id);
+                startActivity(intent);
+            }
+        };
+
+        listDrinks.setOnItemClickListener(itemClickListener);
+
     }
+
 }
