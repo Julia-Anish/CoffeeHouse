f1a702563d12ef1312fbcbf60d56b0cf7b9967d6
Julia Anish <julia.anishchenko@gmail.com>
2020-01-28 15:57:31 +0200

    Made some changes

--- app/src/main/java/com/example/coffeehouse/Food.java
+++ app/src/main/java/com/example/coffeehouse/Food.java
@@ -7,9 +7,9 @@ public class Food {
 
 
     public static final Food [] dessert = {
-            new Food("Pancakes", "Five pancakes made from flour milled in the Smokies. Served with our house made, warm maple syrup, and real whipped butter.", R.drawable.pancakes),
-            new Food("Waffles", "The Original Belgian Sugar Waffle smeared with peanut butter, with or without a dip in Belgian milk chocolate ", R.drawable.waffles),
-            new Food("Panna cotta", "It   is an Italian dessert of sweetened cream thickened with gelatin and molded. The cream may be aromatized with coffee, vanilla, or other flavorings.",R.drawable.pannacotta)
+            new Food("Pancakes", "Five pancakes made from flour milled in the Smokies. Served with our house made, warm maple syrup, and real whipped butter.", R.drawable.pancakes2),
+            new Food("Waffles", "The Original Belgian Sugar Waffle smeared with peanut butter, with or without a dip in Belgian milk chocolate ", R.drawable.waffles2),
+            new Food("Panna cotta", "It   is an Italian dessert of sweetened cream thickened with gelatin and molded. The cream may be aromatized with coffee, vanilla, or other flavorings.",R.drawable.pannacotta2)
 
 
     };
