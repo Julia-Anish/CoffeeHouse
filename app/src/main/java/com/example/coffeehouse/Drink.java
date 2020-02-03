2c5835f34231e9e172404f0c0ab4284dd4afefcf
Julia Anish <julia.anishchenko@gmail.com>
2020-02-03 13:40:19 +0200

    compleet

--- app/src/main/java/com/example/coffeehouse/Drink.java
+++ app/src/main/java/com/example/coffeehouse/Drink.java
@@ -7,8 +7,8 @@ public class Drink {
 
 
     public static final Drink [] drinks = {
-            new Drink("Latte", "It is a coffee-based drink made primarily from espresso and steamed milk. It consists of one-third espresso, two-thirds heated milk and about 1cm of foam. The foam can be poured in such a way to create a picture.", R.drawable.latte),
-            new Drink("Cappuccino", "It is a coffee-based drink made primarily from espresso and milk. It consists of one-third espresso, one-third heated milk and one-third milk foam", R.drawable.cappuccino),
+            new Drink("Latte", "It is a coffee-based drink made primarily from espresso and steamed milk. It consists of one-third espresso, two-thirds heated milk and about 1cm of foam. The foam can be poured in such a way to create a picture.", R.drawable.llatte),
+            new Drink("Cappuccino", "It is a coffee-based drink made primarily from espresso and milk. It consists of one-third espresso, one-third heated milk and one-third milk foam", R.drawable.ccappuccino),
             new Drink("Amaretto", "It is a liqueur with an almond flavor, but surprisingly, it may or may not contain almonds. The standard base of the liqueur is primarily made from either apricot pits or almonds or both.",R.drawable.amaretto),
             new Drink("Green Tea","It is a type of tea that is made from Camellia sinensis leaves and buds that have not undergone the same withering and oxidation process used to make oolong teas and black teas. Green tea originated in China, but its production and manufacture has spread to other countries in East Asia.",R.drawable.greentea)
 
